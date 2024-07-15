372d83f72ad1688222cc4917251197a11316133c
omaryahia4 <omar.yahia4444@gmail.com>
2024-07-15 11:46:49 +0100

    testing app component

--- 0x02-react_intro/task_0/dashboard/src/App.spec.js
+++ 0x02-react_intro/task_0/dashboard/src/App.spec.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react";
 import {render, screen} from "@testing-library/react"
 import App from "./App";
@@ -5,6 +9,25 @@ import App from "./App";
 
 test('renders School Dashboard heading', () => {
     render(<App />);
-    const headingElement = screen.getByText(/School Dashboard/i);
+    const headingElement = screen.getByRole('heading', { name: /School dashboard/i });
     expect(headingElement).toBeInTheDocument();
+
+  });
+
+  test('renders App body text', () => {
+    render(<App />);
+    const bodyElement = screen.getByText(/Login to access the full dashboard/i);
+    expect(bodyElement.closest('.App-body')).toBeInTheDocument();
+  });
+
+  test('renders App footer text', () => {
+    render(<App />);
+    const bodyElement = screen.getByText(/Copyright 2024 - holberton School/i);
+    expect(bodyElement.closest('.App-footer')).toBeInTheDocument();
+  });
+
+  test('renders App img', () => {
+    render(<App />);
+    const imgElement = screen.getByRole('img', { name: /logo/i });
+    expect(imgElement).toBeInTheDocument();
   });
