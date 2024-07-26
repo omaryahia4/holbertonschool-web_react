ba34eaee3a41e6bd370688e928870070e9f85b99
omaryahia4 <omar.yahia4444@gmail.com>
2024-07-26 10:58:39 +0100

    changing name of the repo

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App.jsx';
+
+test('should contain a <p/> element with specific text, <h1/>, and an <img/>', () => {
+  render(<App />);
+
+  const paragraphElement = screen.getByText('Login to access the full dashboard');
+  const footerParagraphElement = screen.getByText(/copyright/i);
+  // this would accept ("School Dashboard", "school dashboard", "SCHOOL DASHBOARD")
+  const headingElement = screen.getByRole('heading', { name: /School dashboard/i });
+  // check whether the img tag exists or not, we can't rely on the alt because its value is not specified in the task.
+  const imgElement = screen.getByRole('img');
+
+  expect(paragraphElement).toBeInTheDocument();
+  expect(footerParagraphElement).toBeInTheDocument();
+  expect(headingElement).toBeInTheDocument();
+  expect(imgElement).toBeInTheDocument();
+});
