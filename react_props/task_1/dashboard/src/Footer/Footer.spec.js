066e92cd3a9b35c740180105aeed309808b61af9
omaryahia4 <omar.yahia4444@gmail.com>
2024-10-10 10:52:04 +0100

    react props test

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,27 +1,8 @@
-/**
- * @jest-environment jsdom
- */
-
-import React from "react";
-import {render, screen} from "@testing-library/react"
-import Footer from "./Footer";
 import { getFooterCopy } from '../utils/utils';
-
-  jest.mock('../utils/utils', () => ({
-    getFooterCopy: jest.fn(),
-    getFullYear: jest.fn()
-  }));
 
   describe('Footer Component', () => {
 
-  test('renders correct copyright string when getFooterCopy returns true', () => {
-    getFooterCopy.mockReturnValue('Holberton School');
-    render(<Footer />);
-    const paragraphElement = screen.getByText((content, element) =>
-      content.startsWith('Copyright') && 
-      content.includes('Holberton School')
-    );
-
-    expect(paragraphElement).toBeInTheDocument();
-  });
+    test('renders correct copyright string when getFooterCopy returns true', () => {
+      expect(getFooterCopy(true)).toBe('Holberton School');
+    });
   })
