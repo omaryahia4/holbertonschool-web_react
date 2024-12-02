5a2c6c0ce0dc8829ab66488f8dc6f6715178d485
omaryahia4 <omar.yahia4444@gmail.com>
2024-12-02 12:01:23 +0100

    react state testing

---
+++ react_state/task_2/dashboard/src/Login/Login.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import WithLogging from '../HOC/WithLogging';
+import './Login.css';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      enableSubmit: false,
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    this.props.logIn(email, password);
+  };
+
+  handleChangeEmail = (e) => {
+    const email = e.target.value;
+    this.setState({ email }, this.validateForm);
+  };
+
+  handleChangePassword = (e) => {
+    const password = e.target.value;
+    this.setState({ password }, this.validateForm);
+  };
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isPasswordValid = password.length >= 8;
+    this.setState({ enableSubmit: isValidEmail && isPasswordValid });
+  };
+
+  render() {
+    const { email, password, enableSubmit } = this.state;
+
+    return (
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <label htmlFor="email" className="email">
+            Email
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={this.handleChangeEmail}
+            />
+          </label>
+          <label htmlFor="password" className="password">
+            Password
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={this.handleChangePassword}
+            />
+          </label>
+          <input
+            className="label-button"
+            type="submit"
+            value="OK"
+            disabled={!enableSubmit}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default WithLogging(Login);
