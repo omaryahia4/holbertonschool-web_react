7b73d40e80015ead476f3a6fce3efea2f6a9189d
omaryahia4 <omar.yahia4444@gmail.com>
2025-01-07 15:19:40 +0100

    react hooks testing

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -11,9 +11,9 @@ const Header = () => {
       <img className="App-logo" src={HolbertonLogo} alt="holberton logo" />
       <h1>School dashboard</h1>
       {user.isLoggedIn && (
-        <section id="logoutSection">
+        <div id="logoutSection">
           Welcome {user.email} (<a href="" onClick={logOut}>logout</a>)
-        </section>
+        </div>
       )}
     </div>
   );
