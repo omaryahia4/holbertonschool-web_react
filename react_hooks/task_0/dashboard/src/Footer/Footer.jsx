5073c8fca3e5451faf0d0bb1f648bb33bbff1079
omaryahia4 <omar.yahia4444@gmail.com>
2025-01-07 15:24:58 +0100

    footer testing

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,23 +1,15 @@
-import React from 'react';
+import { useContext } from 'react';
 import './Footer.css';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
 import { newContext } from '../Context/context';
 
 export default function Footer() {
+  const { user } = useContext(newContext);
+
   return (
-    <newContext.Consumer>
-      {
-        (context) => {
-          if (context.user) {
-            return (
-              <div className="App-footer">
-                <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
-                {context.user.isLoggedIn && <a href="#">Contact us</a>}
-              </div>
-            )
-          }
-        }
-      }
-    </newContext.Consumer>
-  )
+    <div className="App-footer">
+      <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      {user.isLoggedIn && <a href="#">Contact us</a>}
+    </div>
+  );
 }
