e10897ee53a63c6b756466b618df2fdfc81a4a76
omaryahia4 <omar.yahia4444@gmail.com>
2025-01-17 11:08:07 +0100

    task8 testing

---
+++ react_hooks/task_6/dashboard/src/App/appReducer.js
@@ -0,0 +1,62 @@
+export const APP_ACTIONS = {
+    LOGIN: 'LOGIN',
+    LOGOUT: 'LOGOUT',
+    TOGGLE_DRAWER: 'TOGGLE_DRAWER',
+    MARK_NOTIFICATION_READ: 'MARK_NOTIFICATION_READ',
+    SET_NOTIFICATIONS: 'SET_NOTIFICATIONS',
+    SET_COURSES: 'SET_COURSES',
+  };
+
+  export const initialState = {
+    displayDrawer: true,
+    user: { isLoggedIn: false, email: '', password: '' },
+    notifications: [],
+    courses: [],
+  };
+
+  export function appReducer(state = initialState, action) {
+    switch (action.type) {
+    case APP_ACTIONS.LOGIN:
+        return {
+            ...state,
+            user: { ...action.payload },
+        };
+  
+    case APP_ACTIONS.LOGOUT:
+        return {
+            ...state,
+            user: { isLoggedIn: false, email: '', password: '' },
+        };
+  
+      case APP_ACTIONS.TOGGLE_DRAWER:
+        return {
+          ...state,
+          displayDrawer: !state.displayDrawer,
+        };
+  
+      case APP_ACTIONS.MARK_NOTIFICATION_READ:
+        return {
+          ...state,
+          notifications: state.notifications.map((notification) =>
+            notification.id === action.payload.id
+              ? { ...notification, read: true }
+              : notification
+          ),
+        };
+  
+      case APP_ACTIONS.SET_NOTIFICATIONS:
+        return {
+          ...state,
+          notifications: action.payload,
+        };
+  
+      case APP_ACTIONS.SET_COURSES:
+        return {
+          ...state,
+          courses: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  }
