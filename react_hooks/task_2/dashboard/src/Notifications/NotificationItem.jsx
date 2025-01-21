adabefa781df924c520510535886fa71f189bbc1
omaryahia4 <omar.yahia4444@gmail.com>
2025-01-21 11:36:14 +0100

    testing

--- react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,12 +1,12 @@
 import { memo } from 'react';
 
-const NotificationItem = memo(({ type, value, html, markAsRead, id }) => {
+const NotificationItem = memo(({ type, value, html, markAsRead}) => {
   return (
     <li
       style={{ color: type === 'default' ? 'blue' : 'red' }}
       data-notification-type={type}
       dangerouslySetInnerHTML={type === 'urgent' && html !== undefined ? html : undefined}
-      onClick={() => markAsRead(id)}
+      onClick={() => markAsRead()}
     >
       {type === 'urgent' && html !== undefined ? null : value}
     </li>
