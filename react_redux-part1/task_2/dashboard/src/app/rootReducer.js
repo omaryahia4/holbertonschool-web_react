bf9f697030394281606d7485d644408b00295585
omaryahia4 <omar.yahia4444@gmail.com>
2025-01-25 22:05:05 +0100

    fix import statement

--- react_redux-part1/task_2/dashboard/src/app/rootReducer.js
+++ react_redux-part1/task_2/dashboard/src/app/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'; 
-import notificationsReducer from '../features/notifications/notificationsSlicenotificationsSlice'; 
+import notificationsReducer from '../features/notifications/notificationsSlice'; 
 import coursesReducer from '../features/courses/coursesSlice'; 
 
 const rootReducer = combineReducers({
