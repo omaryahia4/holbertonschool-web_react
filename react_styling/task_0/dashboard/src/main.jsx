28cdd735f42332a7ce90160de1ad666f699bce2a
omaryahia4 <omar.yahia4444@gmail.com>
2025-07-01 14:36:25 +0100

    font test

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import './App/App.css';
 import './main.css'
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
